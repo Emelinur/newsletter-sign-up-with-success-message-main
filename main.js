15fdda9b7fec80c46f0337015e2e5014ab29bd7b
Emelinur <emelinuryzgc@gmail.com>
2025-11-27 17:55:57 +0300

    update index.html ,style.css and add main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const newsletterForm=document.querySelector("#newsletter_form");
+const emailInput=document.querySelector(".email_input");
+const errorMsg=document.querySelector(".error-msg");
+const error=document.querySelector(".error");
+const submitButton=document.querySelector(".submit_button");
+
+submitButton.addEventListener("click",(e)=>{
+  e.preventDefault();
+  
+
+});
