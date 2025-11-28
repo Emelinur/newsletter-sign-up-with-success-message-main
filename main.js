887d290a44be905b6cd31c5dd8940160d6f6cd0a
Emelinur <emelinuryzgc@gmail.com>
2025-11-28 17:35:23 +0300

    feat: complete dismiss functionality and reset form state

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const errorMsg = document.querySelector(".error-msg");
 const successMessage = document.querySelector(".card_success_message");
 const card = document.querySelector(".card");
 const placeholderText = document.querySelector(".placeholderText");
+const successBtn = document.querySelector(".success_btn");
 
 newsletterForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -20,4 +21,12 @@ newsletterForm.addEventListener("submit", (e) => {
     card.style.display = "none";
     placeholderText.textContent = inputValue;
   }
+});
+successBtn.addEventListener("click", () => {
+  successMessage.style.display = "none";
+  emailInput.value = "";
+  card.style.display = "flex";
+  emailInput.classList.remove("error");
+  errorMsg.style.display = "none";
+
 });
