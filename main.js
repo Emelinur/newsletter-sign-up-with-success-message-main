2fa2750cfbba6ca1868f0c7c66f0f9aaf5154f74
Emelinur <emelinuryzgc@gmail.com>
2025-11-28 15:12:46 +0300

    feat: add email validation and success message logic

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const newsletterForm=document.querySelector("#newsletter_form");
-const emailInput=document.querySelector(".email_input");
-const errorMsg=document.querySelector(".error-msg");
-const error=document.querySelector(".error");
-const submitButton=document.querySelector(".submit_button");
+const newsletterForm = document.querySelector("#newsletter_form");
+const emailInput = document.querySelector(".email_input");
+const errorMsg = document.querySelector(".error-msg");
+const successMessage = document.querySelector(".card_success_message");
+const card = document.querySelector(".card");
+const placeholderText = document.querySelector(".placeholderText");
 
-submitButton.addEventListener("click",(e)=>{
+newsletterForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  
+  let inputValue = emailInput.value;
+  console.log(inputValue);
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  console.log(emailPattern.test(inputValue));
+  if (inputValue == "" || !emailPattern.test(inputValue)) {
+    errorMsg.style.display = "flex";
+    emailInput.classList.add("error");
+  } else {
+    successMessage.style.display = "flex";
+    card.style.display = "none";
+    placeholderText.textContent = inputValue;
+  }
 });
